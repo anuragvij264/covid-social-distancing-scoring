0743edff26b301a3a63a4f34b476ba6b72c66b78
Vij <avij1@LAMU02X25KLJG5M.uhc.com>
2020-04-21 16:25:54 +0530

    db code

--- db/queries.py
+++ db/queries.py
@@ -1,6 +1,6 @@
 import sqlalchemy as db
 
-engine = db.create_engine("")
+engine = db.create_engine("mysql+mysqldb://user:password@0.0.0.0:3308/db")
 
 def create_tables():
 
@@ -10,9 +10,23 @@ def create_tables():
     images = db.Table('images',metadata,
                       db.Column('id', db.Integer()),
                       db.Column('timestamp',db.DateTime()),
-                      db.Column('user',db.String()),
+                      db.Column('user_name',db.String()),
                       db.Column('media_url',db.String()),
                       db.Column('tweet_url',db.String()),
                       db.Column('output_dir',db.String())
  )
     metadata.create_all(engine)
+
+
+def insert(table_name : str,vals,conn):
+
+
+    query  = db.insert(table_name).values()
+    result = conn.execute(query)
+
+    return result
+
+
+if __name__=='__main__':
+    create_tables()
+
