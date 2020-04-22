d5a154a0ee3a9b05609b9cd259b653c2afe29e59
Jitendra Singh <jitendra.singh@decimal.co.in>
2020-04-22 20:31:00 +0530

    connection string added in python code

--- app/config.py
+++ app/config.py
@@ -1,3 +1,12 @@
 import os
 
 MODEL_PATH = os.path.join("./checkpoints", '0model_best.pth.tar')
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_DATABASE = os.getenv("DB_NAME")
+
+connection_string = "mysql+mysqldb://{}:{}@{}/{}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE)
+
+print(connection_string)
