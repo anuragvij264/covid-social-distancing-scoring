73b90cfa8fd51423c09f013d5c4297eb0e2b1c47
Vij <avij1@LAMU02X25KLJG5M.uhc.com>
2020-04-25 15:34:03 +0530

    twitter bot updated with some queries

--- bot/fetch_images.py
+++ bot/fetch_images.py
@@ -3,9 +3,13 @@ from config import CONFIG_PATH
 from tweepy import OAuthHandler
 import wget
 import os
+import re
 import argparse
 from datetime import datetime, timedelta
 import configparser
+import pandas as pd
+
+from db.queries import engine
 
 def authorize_twitter_api(config):
     auth = OAuthHandler(config['DEFAULT']['consumer_key'], config['DEFAULT']['consumer_secret'])
@@ -13,7 +17,8 @@ def authorize_twitter_api(config):
     return auth
 
 def tweet_media_urls(tweet_status):
-  media = tweet_status._json.get('extended_entities', {}).get('media', [])
+  media = tweet_status._json.get('quoted_status', {}).get('extended_entities', {}).get('media', [])
+
   if (len(media) == 0):
     return []
   else:
@@ -28,8 +33,14 @@ def download(status,num_tweets, output_dir):
     create_folder(output_dir)
     downloaded = 0
 
+    df = pd.DataFrame()
+
+
     stop_date = datetime.utcnow() - timedelta(days=1)
     for tweet_status in status:
+
+        image_urls_list = list()
+        tweet_urls = list()
 
         if (downloaded >= num_tweets):
             break
@@ -37,17 +48,33 @@ def download(status,num_tweets, output_dir):
         # if status.created_at < stop_date:
         #     break
 
+
         loc = tweet_status.full_text
 
-        for count, media_url in enumerate(tweet_media_urls(tweet_status)):
+        tweetId = tweet_status.id
 
+        tweet_url = re.search(r'https:\/\/t.co\/[a-zA-z0-9]+',loc).group()
+
+        loc = re.sub(r'https:\/\/t.co\/[a-zA-z0-9]+','',loc).lower()
+        created = tweet_status.created_at.strftime('%d-%m-%y at %H.%M.%S')
+
+        tweet_urls.append(tweet_url)
+
+        print(loc,tweet_url)
+
+        for count, media_url in enumerate(tweet_media_urls(tweet_status)):
             # Only download if there is not a picture with the same name in the folder already
-            created = tweet_status.created_at.strftime('%d-%m-%y at %H.%M.%S')
+
+            image_urls_list.append(media_url)
             file_name = "{}_({}).jpg".format(created, count + 1)
             if not os.path.exists(os.path.join(output_dir, file_name)):
                 print(media_url)
                 wget.download(media_url, out=output_dir + '/' + file_name)
                 downloaded += 1
+
+        df=pd.concat([df,pd.DataFrame.from_records({'id':int(tweetId),'district':loc,'image_urls':image_urls_list,"created_at": created,"tweet_url": tweet_url})])
+
+        df.to_sql(name='images2', con=engine, if_exists='append', index=False)
 
 
 def download_images_by_user(api, username, retweets, replies, num_tweets, output_folder):
